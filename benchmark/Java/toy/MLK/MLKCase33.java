7e4e10676bd235156d183c473f0208ef5dd9853a
Jiayu Sun <2227683960@qq.com>
2026-03-28 11:16:58 +0800

    new test standard

--- benchmark/Java/toy/MLK/MLKCase33.java
+++
@@ -1,9 +0,0 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class MLKCase33 {
-    public void run() {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // br.close();
-    }
-}
